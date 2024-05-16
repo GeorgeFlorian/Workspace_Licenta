4fccc23612b99f1f5795e523c61ac064e34d04a7
GeorgeFlorian <georgeflorian12@gmail.com>
2024-05-16 14:35:25 +0300

    Style sign out button

--- client/src/components/ui/Header.jsx
+++ client/src/components/ui/Header.jsx
@@ -5,7 +5,11 @@ const LogoutButton = () => {
   let isLoggingOut = fetcher.formData != null;
   return (
     <fetcher.Form method="post" action="/logout">
-      <button type="submit" disabled={isLoggingOut}>
+      <button
+        type="submit"
+        disabled={isLoggingOut}
+        className="flex items-center justify-center bg-blue-500 hover:bg-blue-400 text-blue-200 hover:text-blue-800 rounded-md px-4 py-2 drop-shadow-lg"
+      >
         {isLoggingOut ? "Signing out..." : "Sign out"}
       </button>
     </fetcher.Form>
@@ -14,7 +18,7 @@ const LogoutButton = () => {
 
 export default function Header() {
   return (
-    <header className=" flex items-center justify-end w-full h-24 bg-gray-400 px-8">
+    <header className=" flex items-center justify-end w-full h-24 bg-gray-200 px-8">
       <LogoutButton />
     </header>
   );
