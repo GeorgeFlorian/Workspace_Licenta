45699a7854bd74243779866dee153cb0074105ca
Alexandru Serban <alexandru.serban@itsmartsystems.eu>
2024-06-30 22:23:08 +0300

    added handlesubmit

--- client/src/components/pages/LoginPage.jsx
+++ client/src/components/pages/LoginPage.jsx
@@ -20,6 +20,13 @@ function LoginPage() {
 
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const {login, error, isLoading} = useLogin()
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    await login(email, password)
+  }
 
   return (
     <div className="max-w-md mx-auto mt-8 p-4 bg-white shadow-lg drop-shadow-lg rounded-lg">
